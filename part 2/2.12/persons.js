f16020eef8f92aeaa3be83fd1ee57f2c36b810e0
vincentforsman93 <vincent@ambitiousafrica.org>
2025-10-19 18:01:45 +0300

    Add persons.js with sample contact data

---
+++ part 2/2.12/persons.js
@@ -0,0 +1,8 @@
+const persons = [
+  { id: 1, name: 'Arto Hellas', number: '040-123456' },
+  { id: 2, name: 'Kalle Ankka', number: '39-44-5323523' },
+  { id: 3, name: 'Putin', number: '12-43-234345' },
+  { id: 4, name: 'Trump', number: '39-23-6423122' },
+];
+
+export default persons;
